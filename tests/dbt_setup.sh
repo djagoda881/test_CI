b88433d9879256cb23de74a469ad806e8d827975
djagoda881 <djagoda@dyvenia.com>
2023-01-26 14:34:33 +0100

    ✅ Added xlsx file to tests

--- tests/dbt_setup.sh
+++ tests/dbt_setup.sh
@@ -5,8 +5,9 @@ dbt init postgres
 cp -R ../cli/ postgres/ 
 cp -R ../macros/ postgres/
 cp packages.yml postgres/
+mkdir -p postgres/seeds/master_data/
 cp countries.csv postgres/seeds/master_data/
-cp Average salary.xlsx postgres/seeds/master_data/
+cp "Average salary test.xlsx" postgres/seeds/master_data/
 cp requirements.txt postgres/
 cp setup.sh postgres/
 
