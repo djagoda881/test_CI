9c73daf15273812964cf1f277965effdc43e154c
trymzet <michalmzawadzki@gmail.com>
2023-02-10 13:15:45 +0100

    ♻️ Update `test_model.py`

--- tests/test_model.py
+++ tests/test_model.py
@@ -11,27 +11,11 @@ from nesso.source import check_if_source_exists
 from nesso.source import create as create_source
 
 
-TEST_SOURCE = "public"
-TEST_TABLE_CONTACT_BASE_MODEL = "stg_test_table_contact"
-TEST_TABLE_ACCOUNT_BASE_MODEL = "stg_test_table_account"
-
-MART = "unit_test"
-PROJECT = "unit_test"
-MODEL = "test_model"
-
-MODEL_PATH = DBT_PROJECT_DIR.joinpath(
-    "models", "marts", MART, PROJECT, MODEL, MODEL + ".sql"
-)
-MODEL_YAML_PATH = DBT_PROJECT_DIR.joinpath(
-    "models", "marts", MART, PROJECT, MODEL, MODEL + ".yml"
-)
-
-
-def test_model_bootstrap():
+def test_model_bootstrap(MART, PROJECT, MODEL, MODEL_PATH):
 
     assert not MODEL_PATH.exists()
 
-    bootstrap(MODEL, MART, PROJECT)
+    bootstrap(project=PROJECT, mart=MART, model=MODEL)
 
     assert MODEL_PATH.exists()
 
@@ -39,13 +23,23 @@ def test_model_bootstrap():
     MODEL_PATH.unlink()
 
 
-def test_model_bootstrap_yaml(postgres_connection, create_data_contacts_table):
+def test_model_bootstrap_yaml(
+    postgres_connection,
+    MART,
+    PROJECT,
+    MODEL,
+    MODEL_PATH,
+    TEST_SOURCE,
+    TEST_TABLE_CONTACT,
+    TEST_TABLE_CONTACT_BASE_MODEL,
+    MODEL_YAML_PATH,
+):
 
     assert not check_if_source_exists(TEST_SOURCE)
     assert not check_if_base_model_exists(TEST_TABLE_CONTACT_BASE_MODEL)
     assert not MODEL_YAML_PATH.exists()
 
-    # Creating Source
+    # Create the source and register sources & base models.
     with mock.patch.object(
         builtins,
         "input",
@@ -54,25 +48,30 @@ def test_model_bootstrap_yaml(postgres_connection, create_data_contacts_table):
 
         create_source(TEST_SOURCE)
 
-    # Creating model
-    bootstrap(MODEL, MART, PROJECT)
+    # Materialize the model.
+    bootstrap(MODEL, mart=MART, project=PROJECT)
 
     with open(MODEL_PATH, "a") as f:
         f.write("select * from {{ " + "ref( 'stg_" + TEST_TABLE_CONTACT + "' )" + " }}")
 
     os.system(f"dbt run -m {MODEL}")
 
-    # Creating model.yml file
+    # Bootstrap YAML for the model.
     bootstrap_yaml(
-        MODEL, MART, PROJECT, "test_technical_owner", "test_business_owner", "qa"
+        model=MODEL,
+        mart=MART,
+        project=PROJECT,
+        technical_owner="test_technical_owner",
+        business_owner="test_business_owner",
+        target="qa",
     )
 
     assert MODEL_YAML_PATH.exists()
 
-    # Cleaning up after the test
-    engine.execute(f"DROP VIEW IF EXISTS {MODEL};")
-    engine.execute(f"DROP VIEW IF EXISTS {TEST_TABLE_CONTACT_BASE_MODEL};")
-    engine.execute(f"DROP TABLE IF EXISTS {TEST_TABLE_CONTACT};")
+    # Cleanup.
+    postgres_connection.execute(f"DROP VIEW IF EXISTS {MODEL};")
+    postgres_connection.execute(f"DROP VIEW IF EXISTS {TEST_TABLE_CONTACT_BASE_MODEL};")
+
     shutil.rmtree(
         DBT_PROJECT_DIR.joinpath("models", "sources", TEST_SOURCE),
         ignore_errors=True,
